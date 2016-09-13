e4bc9a0720adb55bc3b8ed477c3d14216efe2968
Matt Latham <latham75@gmail.com>
2016-09-13 10:54:13 -0500

    fixe port number, added filter and name search

--- index.js
+++ index.js
@@ -1,40 +1,48 @@
 'use strict';
 
 const {MongoClient} = require('mongodb')
-const MongoDB_url = 'mongodb://localhost:27107/test'
-
+const MongoDB_url = 'mongodb://localhost:27017/test'
+const {argv: [,,...filter]} = process
 // MongoClient.connect(MongoDB_url, (err, db) => {
 //   console.log(db)
 //   db.close() //will close mongodb connection when done
 // })
+
+const name = RegExp(`^${filter.join(' ')}`, 'i')
 
 //way one
 MongoClient
   .connect(MongoDB_url)
   .then(db => {  //returns promise since no callback passed
     db.collection('restaurants')
-      .find()
-      .toArray()
-      .then((restaurants) => {
-        restaurants.forEach(restaurant => {
-          console.log(restaurant)
-        })
-      })
-      .then(() => db.close())
+      .find({name})
+      .sort({name:1})
+      // .toArray()
+      // .then((restaurants) => {
+      //   restaurants.forEach(restaurant => {
+      //     console.log(restaurant)
+      //   })
+      // })
+      // .then(() => db.close())
+    .forEach(restaurant => {
+        if (restaurant.name) {
+          console.log(restaurant.name)
+        }
+      }, () => db.close())
   })
   .catch(console.error)
 
-//way two
-MongoClient
-  .connect(MongoDB_url)
-  .then(db => {  //returns promise since no callback passed
-    db.collection('restaurants')
-      .find()
-      // .toArray()
-      .forEach((restaurant) => { //goes over all docs
-        console.log(restaurant)
-      },
-      () => db.close())
-  })
-  .catch(console.error)
+// //way two
+// MongoClient
+//   .connect(MongoDB_url)
+//   .then(db => {  //returns promise since no callback passed
+//     db.collection('restaurants')
+//       .find()
+//       // .toArray()
+//       .forEach((restaurant) => { //goes over all docs
+//         console.log(restaurant)
+//       },
+//       () => db.close())
+//   })
+//   .catch(console.error)
 
